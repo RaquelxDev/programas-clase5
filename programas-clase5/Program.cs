3345afafb8048b7a41834c8b438a8bcdb3bbbe83
raquelxdev <raquel.perez.0147@gmail.com>
2024-03-22 01:22:17 -0600

    correcciones

--- programas-clase5/Program.cs
+++ programas-clase5/Program.cs
@@ -1,4 +1,5 @@
-﻿static void suma()
+﻿/*
+static void suma()
 {
     //ejercicio 1
     int num1, num2, resultado = 0;
@@ -7,7 +8,7 @@
 
     Console.WriteLine("Ingrese num2: ");
     num2 = Convert.ToInt32(Console.ReadLine());
-
+    
     resultado = num1 + num2;
 }
 
@@ -43,3 +44,84 @@ int promedio = sumatoria(20, 25, 40) / 3;
 Console.WriteLine($"hola {nombre} su promedio es {promedio} su edad es");
 
 
+//Adivinar un número que está pensando
+
+Console.WriteLine("- - - Piensa en un número - - - ");
+
+Console.Write("\nIngresa un Numero: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("\nMultiplica tu numero por 2");
+int Resultado = numero * 2;
+
+Console.WriteLine("\nSumale 8");
+Resultado += 8;
+
+Console.WriteLine("\nMultiplicalo por 5");
+Resultado *= 5;
+
+Console.Write("\nIngresa el resultado final: ");
+int ResultFinal = int.Parse(Console.ReadLine()!);
+
+string SegundoResultado = ResultFinal.ToString();
+SegundoResultado = SegundoResultado.Substring(0, SegundoResultado.Length - 1);
+
+ResultFinal = int.Parse(SegundoResultado) - 4;
+
+Console.WriteLine($"\nEl número que pensaste es {ResultFinal}");
+
+
+//Contador de palabras
+Console.WriteLine("Ingrese una frase ");
+string Frase = Console.ReadLine()!;
+
+Console.WriteLine("La frase ingresada fue:  " + Frase);
+
+int Palabras = Frase.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+int Vocales = Contadorvocales(Frase);
+
+Console.WriteLine($"El numero de palabras ingresadas: {Palabras}");
+Console.WriteLine($"El numero de vocales ingresadas: {Vocales}");
+
+static int Contadorvocales(string Frase)
+{
+    int contador = 0;
+    foreach (char c in Frase.ToLower())
+    {
+        if ("aeiou".Contains(c))
+        {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+
+
+//Palíndromos
+
+Console.WriteLine("Ingrese una palabra ");
+string palabra = Console.ReadLine()!.ToLower();
+
+bool Palindromo = true;
+
+for (int i = 0; i < palabra.Length / 2; i++)
+{
+    if (palabra[i] != palabra[palabra.Length - 1 - i])
+    {
+        Palindromo = false;
+        break;
+    }
+}
+
+if (Palindromo)
+{
+    Console.WriteLine("La palabra ingresada es un palíndromo");
+}
+else
+{
+    Console.WriteLine("La palabra ingresada no es un palíndromo");
+}
+
+
+*/
